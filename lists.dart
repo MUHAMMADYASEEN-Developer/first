50a3e03a2e4e5463206c001481013dd3b29e1981
MUHAMMADYASEEN-Developer <muhammadyaseen692006@gmail.com>
2024-07-28 23:49:29 +0500

    name

--- lists.dart
+++ lists.dart
@@ -1,16 +1,3 @@
-main(){
-    List<String> names = ['muhammad','yaseen','ali'];
-    print(names);
-
-
-  List<String> daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  print(daysOfWeek[0]);
-
-
-  List studentData = ['Yaseen', 10, 33, 85.5]; 
-  
-  print('Name: ${studentData[0]}');
-  print('Class: ${studentData[1]}');
-  print('Roll No.: ${studentData[2]}');
-  print('Grade Percentage: ${studentData[3]}');
+main() {
+  print("Muhammad Yaseen");
 }
